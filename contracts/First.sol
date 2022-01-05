de482f7f5ef3a13c69b416717c376622a41d6008
Muhammad Salif <muhammad.salif@yahoo.com>
2022-01-05 13:58:33 +0500

    compiling migration done.

---
+++ contracts/First.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract First {
+    function showMessage() public view returns (string memory) {
+        return "Hello World";
+    }
+}
